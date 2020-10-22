e60633c0554d89601d0d1a8a05ad16d7cc65f007
Bernat Borrás <bernat.borras@adevinta.com>
2020-10-22 20:00:17 +0200

    Last commit

--- app/src/main/java/com/alorma/multithemes/MainActivity.kt
+++ app/src/main/java/com/alorma/multithemes/MainActivity.kt
@@ -3,8 +3,11 @@ package com.alorma.multithemes
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import androidx.annotation.ColorRes
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.Toolbar
+import androidx.appcompat.content.res.AppCompatResources
+import com.google.android.material.card.MaterialCardView
+import com.google.android.material.color.MaterialColors
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,6 +21,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         setSupportActionBar(findViewById(R.id.toolbar))
+
+        val card1 = findViewById<MaterialCardView>(R.id.card1)
+        val card2 = findViewById<MaterialCardView>(R.id.card2)
+        val card3 = findViewById<MaterialCardView>(R.id.card3)
+
+        updateCardColor(card1, R.color.color_primary_8)
+        updateCardColor(card2, R.color.color_primary_50)
+        updateCardColor(card3, R.color.color_primary)
+    }
+
+    private fun updateCardColor(card: MaterialCardView, @ColorRes colorStateListRes: Int) {
+        val color = AppCompatResources.getColorStateList(this, colorStateListRes).defaultColor
+
+        val backgroundColor = MaterialColors.getColor(card, R.attr.colorSurface)
+
+        val newColor = MaterialColors.layer(
+            backgroundColor,
+            color,
+        )
+
+        card.setCardBackgroundColor(newColor)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
